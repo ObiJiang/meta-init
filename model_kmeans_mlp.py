bdcf8eb52acc52a6d5e6b8ed066733e3367f13b1
Yibo Jiang <jyb940413@gmail.com>
2019-05-28 23:52:53 -0700

    fashine mnist with farce labeling and no batch norm

--- model_kmeans_mlp.py
+++ model_kmeans_mlp.py
@@ -273,7 +273,7 @@ if __name__ == '__main__':
 				centriod_list = []
 				for _ in range(config.batch_size):
 					if config.mnist_train:
-						data_one, labels_one = generator.generate(metaCluster.num_sequence, metaCluster.fea, metaCluster.k, pool_type='EASY_TRAIN')
+						data_one, labels_one = generator.generate(metaCluster.num_sequence, metaCluster.fea, metaCluster.k, pool_type='HALF_TRAIN')
 						centriod_one = np.expand_dims(metaCluster.get_k_means_center(data_one), axis=0)
 						data_one = np.expand_dims(data_one, axis=0)
 						labels_one = np.expand_dims(labels_one, axis=0)
@@ -296,7 +296,7 @@ if __name__ == '__main__':
 					centriod_list = []
 					for _ in range(config.batch_size):
 						if config.mnist_train:
-							data_one, labels_one = generator.generate(metaCluster.num_sequence, metaCluster.fea, metaCluster.k, is_train=False, pool_type='EASY_TEST')
+							data_one, labels_one = generator.generate(metaCluster.num_sequence, metaCluster.fea, metaCluster.k, is_train=False, pool_type='HALF_TEST')
 							centriod_one = np.expand_dims(metaCluster.get_k_means_center(data_one), axis=0)
 							data_one = np.expand_dims(data_one, axis=0)
 							labels_one = np.expand_dims(labels_one, axis=0)
@@ -340,7 +340,7 @@ if __name__ == '__main__':
 				metaCluster.max_iter = itr
 				for _ in range(100):
 					# data, labels, centriods = metaCluster.create_dataset()
-					data, labels = generator.generate(metaCluster.num_sequence, metaCluster.fea, metaCluster.k, is_train=False, pool_type='EASY_TEST')
+					data, labels = generator.generate(metaCluster.num_sequence, metaCluster.fea, metaCluster.k, is_train=False, pool_type='HALF_TEST')
 					data = np.squeeze(data)
 					labels = np.squeeze(labels)
 
